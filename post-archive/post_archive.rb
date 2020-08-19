4cb6b7b56061b99772419e85f32008709dc2e6c3
Arnei <arnepokemon@yahoo.de>
2020-08-19 10:03:39 +0200

    Rewrote regex matching to support Ruby version before 2.4

--- post-archive/post_archive.rb
+++ post-archive/post_archive.rb
@@ -809,10 +809,10 @@ end
 
 # Create Mediapackage
 uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-if !dc_data[:identifier].to_s.empty? && uuid_regex.match?(dc_data[:identifier].to_s.downcase)
+if !dc_data[:identifier].to_s.empty? && (dc_data[:identifier].to_s.downcase =~ uuid_regex)
   mediapackage = requestIngestAPI(:put, '/ingest/createMediaPackageWithID/' + dc_data[:identifier], DEFAULT_REQUEST_TIMEOUT,{})
 else
-  if !dc_data[:identifier].to_s.empty? && !uuid_regex.match?(dc_data[:identifier].to_s.downcase)
+  if !dc_data[:identifier].to_s.empty? && !(dc_data[:identifier].to_s.downcase =~ uuid_regex)
     BigBlueButton.logger.warn(" The given identifier #{dc_data[:identifier]} is not a valid UUID. Using generated UUID instead.")
   end
   mediapackage = requestIngestAPI(:get, '/ingest/createMediaPackage', DEFAULT_REQUEST_TIMEOUT, {})
