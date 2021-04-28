fde43009f97698c6de2bfdbcb3111d8cc5a30e93
Arnei <arnewilken@yahoo.de>
2021-04-28 11:49:36 +0200

    Fixed "created" Metadata not defaulting to meeting start time

--- post-archive/post_archive.rb
+++ post-archive/post_archive.rb
@@ -658,8 +658,8 @@ BigBlueButton.logger.info( "Sorted tracks: ")
 BigBlueButton.logger.info( tracks)
 
 # Create metadata file dublincore
-dc_data = OcDublincore::parseDcMetadata(meeting_metadata, server: $oc_server, user: $oc_user, password: $oc_password)
-dublincore = OcDublincore::createDublincore(dc_data)
+dc_data = OcDublincore::parseDcMetadata(meeting_metadata, startTime: real_start_time, endTime: real_end_time,
+                                        server: $oc_server, user: $oc_user, password: $oc_password)dublincore = OcDublincore::createDublincore(dc_data)
 BigBlueButton.logger.info( "Dublincore: \n" + dublincore.to_s)
 
 # Create Json containing cutting marks at path
